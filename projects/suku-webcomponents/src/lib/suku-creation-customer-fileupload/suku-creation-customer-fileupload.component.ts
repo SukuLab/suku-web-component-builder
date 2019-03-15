c4e4fbc224d98d35a54b3ca2cee907f904e7f57a
paulpinto88 <pintonpaul@gmail.com>
2019-03-15 18:30:19 +0530

    hot fix file upload

--- projects/suku-webcomponents/src/lib/suku-creation-customer-fileupload/suku-creation-customer-fileupload.component.ts
+++ projects/suku-webcomponents/src/lib/suku-creation-customer-fileupload/suku-creation-customer-fileupload.component.ts
@@ -7,16 +7,29 @@ import { FormGroup, FormBuilder, FormsModule, FormControl, Validators } from '@a
   styleUrls: ['./suku-creation-customer-fileupload.component.scss']
 })
 export class SukuCreationCustomerFileuploadComponent implements OnInit {
-  imagepath;
-  showMessage;
-  showPdfErrorMsg;
+  
   @Output() onFileChange = new EventEmitter();
   @Output() submitData = new EventEmitter();
+  imagepath;
+  showPdfErrorMsg;
+  showMessage;
 
   constructor() { }
 
   ngOnInit() {
     console.log("suku-creation-customer-fileupload");
   }
-
+  fileupload(e){
+    console.log(e)
+    const files = e.target.files;
+		const maxFileSize = 2100000;
+		let currentFileSize;
+		if (e.target.files && e.target.files.length > 0) {
+			for (let i = 0; i < e.target.files.length; i++) {
+				const file = e.target.files[i];
+				this.onFileChange.emit(file);
+				currentFileSize = file.size;
+			}
+		}
+  }
 }
