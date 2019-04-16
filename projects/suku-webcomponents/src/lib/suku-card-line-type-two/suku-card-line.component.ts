cf739858c34ec05e902d48509d8994d89061ce08
parashur <parashuramb1995@gmail.com>
2019-04-16 16:38:07 +0530

    card-line comp update

--- projects/suku-webcomponents/src/lib/suku-card-line-type-two/suku-card-line.component.ts
+++ projects/suku-webcomponents/src/lib/suku-card-line-type-two/suku-card-line.component.ts
@@ -17,9 +17,8 @@ export class SukuCardLineTypeTwoComponent implements OnInit {
 
 	@Input() interest;
 	@Input() imageColSize = "col-sm-1 pt-1 pl-2";
-	@Input() txtcolsize = "col-sm-4 pl-sm-5 pt-2 mt-1";
-	@Input() txttwocolsize = "col-sm-2 pt-2 pl-lg-2 pr-lg-2 mt-1";
-	@Input() txtthreecolsize = "col-sm-3 pt-2 mt-1";
+	@Input() txtcolsize = "col-sm-6 pl-sm-5 pt-2 mt-1";
+	@Input() txttwocolsize = "col pt-2 pl-lg-2 pr-lg-2 mt-1";
 	@Input('title-one') titleOne = 'Title One';
 	@Input('title-one-color') titleOneColor = 'black';
 	@Input('title-one-weight') titleOneWeight = '600';
@@ -41,17 +40,6 @@ export class SukuCardLineTypeTwoComponent implements OnInit {
 	@Input('sub-title-two-weight') subTitleTwoWeight = '500';
 	@Input('sub-title-two-size') subTitleTwoSize = '12';
   @Input('sub-title-two-customclass') subTitleTwocustomclass = '';
-
-	@Input('sub-title-three') subTitleThree = 'EXPIRY DATE';
-	@Input('sub-title-three-color') subTitleThreecolor = '#b6b6b6';
-	@Input('sub-title-three-weight') subTitleThreeWeight = '500';
-	@Input('sub-title-three-size') subTitleThreeSize = '12';
-  @Input('sub-title-threeclass') subTitleThreeClass = '';
-	@Input('content-three') contentThree = '28 Nov 2018';
-	@Input('content-three-color') contentThreeColor = '#3e3e3e';
-	@Input('content-three-widget') contentThreeWeight = '500';
-	@Input('content-three-size') contentThreeSize = '14';
-  @Input('content-three-customclass') contentThreecustomclass = '';
 
   constructor() { }
 
