2e2eebeac79fcdf449bc965549b0555e0ca4f872
parashur <parashuramb1995@gmail.com>
2019-04-17 16:56:46 +0530

    rating comp update

--- projects/suku-webcomponents/src/lib/suku-rating-card-line/suku-rating-card-line.component.ts
+++ projects/suku-webcomponents/src/lib/suku-rating-card-line/suku-rating-card-line.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
 @Component({
 	selector: 'suku-rating-card-line',
 	templateUrl: './suku-rating-card-line.component.html',
-	styleUrls: [ './suku-rating-card-line.component.scss' ]
+	styleUrls: ['./suku-rating-card-line.component.scss']
 })
 export class SukuRatingCardLineComponent implements OnInit {
 	fullStars = 0;
@@ -21,7 +20,6 @@ export class SukuRatingCardLineComponent implements OnInit {
 	@Input('customImgclass') customImgclass = 'img-res';
 	@Input('custom-iconclass') customIconclass = 'text-center';
 	@Input() bgColor = '';
-
 	@Input() interest;
 	@Input() imageColSize = 'col-sm-1 p-0';
 	@Input() txtcolsize = 'col-sm-3 col-xs-12 pl-lg-2 pr-2 pt-1 mt-1';
@@ -32,20 +30,17 @@ export class SukuRatingCardLineComponent implements OnInit {
 	@Input('title-one-weight') titleOneWeight = '600';
 	@Input('title-one-size') titleOneSize = '14';
 	@Input('title-one-customclass') titleOnecustomclass = '';
-
-	@Input('rating-title') ratingTitle = 'TRANSACTION RATING';
+	@Input('rating-title') ratingTitle = 'RATING RECEIVED';
 	@Input('rating-title-color') ratingtitleColor = '#b6b6b6';
 	@Input('rating-title-weight') ratingtitleWeight = '500';
 	@Input('rating-title-size') ratingtitleSize = '12';
 	@Input('rating-title-customclass') ratingtitlecustomclass = '';
-
-	@Input('content-one') contentOne = '$100';
+	@Input('content-one') contentOne = 'N/A';
 	@Input('content-one-color') contentOneColor = '#3e3e3e';
 	@Input('content-one-weight') contentOneWeight = '500';
 	@Input('content-one-size') contentOneSize = '14';
 	@Input('content-one-customclass') contentOnecustomclass = '';
-
-	@Input('content-two') contentTwo = 'John Smith';
+	@Input('content-two') contentTwo = 'N/A';
 	@Input('content-two-color') contentTwoColor = '#3e3e3e';
 	@Input('content-two-weight') contentTwoWeight = '500';
 	@Input('content-two-size') contentTwoSize = '14';
@@ -55,13 +50,12 @@ export class SukuRatingCardLineComponent implements OnInit {
 	@Input('sub-title-two-weight') subTitleTwoWeight = '500';
 	@Input('sub-title-two-size') subTitleTwoSize = '12';
 	@Input('sub-title-two-customclass') subTitleTwocustomclass = '';
-
-	@Input('sub-title-three') subTitleThree = 'EXPIRY DATE';
+	@Input('sub-title-three') subTitleThree = 'TOTAL PRICE';
 	@Input('sub-title-three-color') subTitleThreecolor = '#b6b6b6';
 	@Input('sub-title-three-weight') subTitleThreeWeight = '500';
 	@Input('sub-title-three-size') subTitleThreeSize = '12';
 	@Input('sub-title-threeclass') subTitleThreeClass = '';
-	@Input('content-three') contentThree = '28 Nov 2018';
+	@Input('content-three') contentThree = 'N/A';
 	@Input('content-three-color') contentThreeColor = '#3e3e3e';
 	@Input('content-three-widget') contentThreeWeight = '500';
 	@Input('content-three-size') contentThreeSize = '14';
@@ -79,8 +73,6 @@ export class SukuRatingCardLineComponent implements OnInit {
 		this.emptyStars = 5 - this.fullStars - this.halfStar;
 		this.emptyStarsArray = Array(this.emptyStars).fill(1);
 	}
-
-	constructor() {}
-
-	ngOnInit() {}
+	constructor() { }
+	ngOnInit() { }
 }
