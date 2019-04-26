a99707c37057d3b81a4c5a47b330d3b6b9eef322
aashif95 <aashifrahuman@comorin.co>
2019-04-26 17:25:03 +0530

    Transaction history UI updates

--- projects/suku-webcomponents/src/lib/suku-transaction-history/suku-transaction-history.component.ts
+++ projects/suku-webcomponents/src/lib/suku-transaction-history/suku-transaction-history.component.ts
@@ -1,38 +1,37 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
-  selector: 'suku-transaction-history',
-  templateUrl: './suku-transaction-history.component.html',
-  styleUrls: ['./suku-transaction-history.component.scss']
+	selector: 'suku-transaction-history',
+	templateUrl: './suku-transaction-history.component.html',
+	styleUrls: [ './suku-transaction-history.component.scss' ]
 })
 export class SukuTransactionHistoryComponent implements OnInit {
-  @Input() transactionHistory = 'Transaction History'
-  @Input() transactionDetailsId = 'transactionDetailsId';
-  @Input() transactionDetails;
-  @Input() headings;
-  @Input() typeId = 'typeId';
-  @Input() myAccountId = 'myAccountId';
-  @Input() counterPartyId = 'counterPartyId';  @Input() counterparty = 'Counter Party';
-  @Input() amountId = 'AmountId';
-  @Input() statusId = 'statusId';
-  @Input() dateCreatedId = 'dateCreatedId';
-  @Input() paymentManagementId = 'paymentManagementId';
-  @Output() action = new EventEmitter();
-  @Input() cancelId = 'cancelId';
-  @Input() cancel = 'Cancel';
-  constructor() {
-  }
+	@Input() transactionHistory = 'Transaction History';
+	@Input() transactionDetailsId = 'transactionDetailsId';
+	@Input() transactionDetails;
+	@Input() heading;
+	@Input() typeId = 'typeId';
+	@Input() myAccountId = 'myAccountId';
+	@Input() counterPartyId = 'counterPartyId';
+	@Input() counterparty = 'Counter Party';
+	@Input() amountId = 'AmountId';
+	@Input() statusId = 'statusId';
+	@Input() dateCreatedId = 'dateCreatedId';
+	@Input() paymentManagementId = 'paymentManagementId';
+	@Output() action = new EventEmitter();
+	@Input() cancelId = 'cancelId';
+	@Input() cancel = 'Cancel';
+	constructor() {}
 
-  ngOnInit() {
-    this.headings = [{
-      'type': 'Type',
-      'myAccount': 'My Account',
-      'counterparty': 'Counter Party',
-      'amount': 'Amount',
-      'status': 'Status',
-      'dateInitiated': 'Initiated On',
-      'dateCreated': 'Completed On',
-      'paymentManagement': 'Payment Management'
-    }]
-  }
-
+	ngOnInit() {
+		this.heading = {
+			type: 'Type',
+			myAccount: 'My Account',
+			counterparty: 'Counterparty',
+			amount: 'Amount',
+			status: 'Status',
+			dateInitiated: 'Initiated On',
+			dateCreated: 'Completed On',
+			paymentManagement: 'Payment Management'
+		};
+	}
 }
