a492ed503e5a580591aca6896f3c930589a2d4e2
parashur <parashuramb727@gmail.com>
2019-05-06 17:40:58 +0530

    help and trx his updates

--- projects/suku-webcomponents/src/lib/suku-payment-portal-help/suku-payment-portal-help.component.ts
+++ projects/suku-webcomponents/src/lib/suku-payment-portal-help/suku-payment-portal-help.component.ts
@@ -1,18 +1,15 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'suku-payment-portal-help',
-  templateUrl: './suku-payment-portal-help.component.html',
-  styleUrls: ['./suku-payment-portal-help.component.scss']
+	selector: 'suku-payment-portal-help',
+	templateUrl: './suku-payment-portal-help.component.html',
+	styleUrls: [ './suku-payment-portal-help.component.scss' ]
 })
 export class SukuPaymentPortalHelpComponent implements OnInit {
-
-  constructor() { }
-  @Input() helpPage = 'FIND ALL INFORMATION REQUIRED FOR THE HELP PAGE';
-  @Input() helpPageHref = 'https://docs.google.com/document/d/11BYv2KVQSEfProqHHxvoxEsOyxZwuqNu2UXTQnSQS14/edit#heading=h.l7cgm61nvh7m';
-  @Output() add = new EventEmitter();
-  ngOnInit() {
-
-  }
-
+	constructor() {}
+	@Input() helpPage = 'FIND ALL INFORMATION REQUIRED FOR THE HELP PAGE';
+	@Input()
+	helpPageHref = 'https://docs.google.com/document/d/11BYv2KVQSEfProqHHxvoxEsOyxZwuqNu2UXTQnSQS14/edit#heading=h.l7cgm61nvh7m';
+	@Output() add = new EventEmitter();
+	ngOnInit() {}
 }
