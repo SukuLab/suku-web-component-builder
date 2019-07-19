f836397759a98fe56c17fb862b8ad3b357e74104
parashur <parashuram.b@comorin.co>
2019-07-19 20:22:04 +0530

    suku-transaction webcomp updates SukuLab/suku-web-component-builder#8

--- projects/suku-webcomponents/src/lib/suku-transaction-history/suku-transaction-history.component.ts
+++ projects/suku-webcomponents/src/lib/suku-transaction-history/suku-transaction-history.component.ts
@@ -2,13 +2,27 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
 	selector: 'suku-transaction-history',
 	templateUrl: './suku-transaction-history.component.html',
-	styleUrls: [ './suku-transaction-history.component.scss' ]
+	styleUrls: ['./suku-transaction-history.component.scss']
 })
 export class SukuTransactionHistoryComponent implements OnInit {
-	@Input() transactionHistory = 'Transaction History';
-	@Input() transactionDetailsId = 'transactionDetailsId';
-	@Input() transactionDetails;
+	@Input('transaction-history') transactionHistory = 'Transaction History';
+	@Input('transaction-details-id') transactionDetailsId = 'transactionDetailsId';
 	@Input() heading;
+	@Input('custom-class') customClass = 'historyWidget p-4 pt-2 pb-3 text-white text-center ';
+	@Input('table-bg-color') tableBgColor = 'tableBg mb-0 pl-4 pr-5';
+	@Input('transaction-history-tittle') transactionHistoryTittle = 'pop text-center mb-0';
+	@Input('bg-custom-class') bgCustomClass = 'box-shadows mb-3 p-0';
+	@Input('spinner-custom-class') spinnerCustomClass = 'justify-content-center';
+	@Input('spinner-radius') spinnerRadius = '65';
+	@Input('spinner-color') spinnerColor = 'primary';
+	@Input('loading-custom-class') loadingCustomClass = 'mt-4 pb-3 col-sm-12 text-center';
+	@Input('loader-message') loaderMessage = 'Please wait... ';
+	@Input('loading-color') loadingColor;
+	@Input('loading-size') loadingSize;
+	@Input('loading-weight') loadingWeight;
+	@Input() tableHeaderCustomClass = 'text-center';
+	@Input() tableBodyCustomClass = 'text-left';
+	@Input() buttonCustomClass = 'btn  btnCancel';
 	@Input() typeId = 'typeId';
 	@Input() myAccountId = 'myAccountId';
 	@Input() counterPartyId = 'counterPartyId';
@@ -20,7 +34,10 @@ export class SukuTransactionHistoryComponent implements OnInit {
 	@Output() action = new EventEmitter();
 	@Input() cancelId = 'cancelId';
 	@Input() cancel = 'Cancel';
-	constructor() {}
+	@Input() transactionDetails = [];
+	constructor() { }
+
+
 
 	ngOnInit() {
 		this.heading = {
