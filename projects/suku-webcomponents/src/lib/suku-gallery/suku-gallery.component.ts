f7a1dc1bd03316bd485a95791d8dc8a4681587ef
parashur <parashuramb727@gmail.com>
2019-07-26 09:45:55 +0530

    suku-gallery #10

--- projects/suku-webcomponents/src/lib/suku-gallery/suku-gallery.component.ts
+++ projects/suku-webcomponents/src/lib/suku-gallery/suku-gallery.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  HostListener,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'suku-gallery',
@@ -44,31 +51,64 @@ export class SukuGalleryComponent implements OnInit {
         index: 'one',
         type: 'image',
         checked: true,
-        link: 'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        link:
+          'https://leiscreekcattleco.com/wp-content/uploads/2018/10/WhyAkaushiPhoto.jpg'
       },
       {
         index: 'two',
         type: 'image',
         checked: false,
-        link: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
+        link:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaurornWO8dpOQVsH3hxOjonAzHbbcMsOA0g6_ziGZVT92HBsP'
       },
       {
         index: 'three',
         type: 'video',
         checked: false,
-        link: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+        link:
+          'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
       },
       {
         index: 'four',
         type: 'image',
         checked: false,
-        link: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
+        link:
+          'https://www.uaex.edu/farm-ranch/animals-forages/images/baldy%20with%20calf%20square.JPG'
       },
       {
         index: 's',
         type: 'video',
         checked: false,
-        link: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+        link:
+          'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+      },
+      {
+        index: 'one',
+        type: 'image',
+        checked: true,
+        link:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaXN6xiu-IvpxOb93cBuTrqJwXM5IcMStHAMKpMLXx7K6qfENb'
+      },
+      {
+        index: 'one',
+        type: 'image',
+        checked: true,
+        link:
+          'https://upload.wikimedia.org/wikipedia/commons/f/f6/Limia_cattle.jpg'
+      },
+      {
+        index: 'one',
+        type: 'image',
+        checked: true,
+        link:
+          'http://www.broken-spur-ranch.com/Sites/10/image/Zabaco%20Feb.%20-%20Copy.jpg'
+      },
+      {
+        index: 'two',
+        type: 'image',
+        checked: false,
+        link:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLIJXakzsFcGAsLMqlbwWFZtZqrliH6rJN6HdWAHPfW36EQF08'
       }
     ];
     this.start();
@@ -79,7 +119,6 @@ export class SukuGalleryComponent implements OnInit {
     this.videoElement.nativeElement.pause();
     this.autoplay = false;
     this.gallery.forEach((element, index) => {
-
       // if (i == this.gallery[this.gallery.length]) {
       //   this.nextBtnVisible = false;
       //   this.prevBtnVisible = true;
@@ -93,7 +132,6 @@ export class SukuGalleryComponent implements OnInit {
       // } else {
       //   this.prevBtnVisible = true;
       // }
-
 
       if (position == 'prev') {
         if (i > 0) {
@@ -122,8 +160,32 @@ export class SukuGalleryComponent implements OnInit {
         }
         console.log('ele', i, position, element);
       }
-
     });
+  }
+
+  preview(e) {
+    // Get the modal
+    const modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    const img = document.getElementById('myImg');
+    const modalImg = <HTMLInputElement>document.getElementById('img01');
+    const modalVid = <HTMLInputElement>document.getElementById('vid');
+    const captionText = document.getElementById('caption');
+    // img.onclick = function() {
+    modal.style.display = 'block';
+    if ((e.type == 'image')) {
+      modalImg.src = e.link;
+    } else {
+      modalVid.src = e.link;
+    }
+    //   captionText.innerHTML = this.alt;
+  }
+
+  closeModal() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+    const span = document.getElementsByClassName('close')[0];
   }
 
   start() {
