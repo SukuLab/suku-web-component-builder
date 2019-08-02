93cfc461b4dec3129db35fdc5c2df9ed73b3fdc3
paulpinto88 <pintonpaul@gmail.com>
2019-08-02 19:35:49 +0530

    Suku-modal fix

--- projects/suku-webcomponents/src/lib/suku-modal/suku-modal.service.ts
+++ projects/suku-webcomponents/src/lib/suku-modal/suku-modal.service.ts
@@ -8,22 +8,14 @@ export class SukuModalService {
 	public confirmationDialogWidth;
 	public confirmationDialogHeight;
 	public confirmationDialogClose;
-	// public onDialogClose: EventEmitter<any> = new EventEmitter<any>();
-	public onDialogCloseInterest: EventEmitter<any> = new EventEmitter<any>();
-	public onDialogCloseList: EventEmitter<any> = new EventEmitter<any>();
-	public onDialogClosePlaceBid: EventEmitter<any> = new EventEmitter<any>();
-	public onDialogCloseAcceptBid: EventEmitter<any> = new EventEmitter<any>();
-	public onDialogCloseSendPayment: EventEmitter<any> = new EventEmitter<any>();
-	public	onDialogCloseDeleteList: EventEmitter<any> = new EventEmitter<any>();
-	public onDialogCloseCancelBid: EventEmitter<any> = new EventEmitter<any>();
-	public onDialogCloseUpdateInterest: EventEmitter<any> = new EventEmitter<any>();
+	public onDialogClose: EventEmitter<any> = new EventEmitter<any>();
 
 	constructor(public dialogService: MatDialog) {
 		this.confirmationDialogHeight = 'auto';
 		this.confirmationDialogWidth = '500px';
 	}
 
-	public openConfirmationDialogInterest(data?) {
+	public openConfirmationDialog(data?) {
 		const dialogRef = this.dialogService.open(SukuConfirmationComponent, {
 			width: this.confirmationDialogWidth,
 			height: this.confirmationDialogHeight,
@@ -46,192 +38,7 @@ export class SukuModalService {
 			}
 		});
 		dialogRef.afterClosed().subscribe((result) => {
-			this.onDialogCloseInterest.emit(result);
-		});
-	}
-
-	public openConfirmationDialogList(data?) {
-		const dialogRef = this.dialogService.open(SukuConfirmationComponent, {
-			width: this.confirmationDialogWidth,
-			height: this.confirmationDialogHeight,
-			disableClose: this.confirmationDialogClose,
-			data: {
-				icon: data ? data.icon : '',
-				iconCustomClass: data ? data.iconCustomClass : '',
-				iconId: data ? data.iconId : '',
-				titleOne: data ? data.titleOne : '',
-				titleOneId: data ? data.titleOneId : '',
-				titleTwo: data ? data.titleTwo : '',
-				titleTwoId: data ? data.titleOneId : '',
-				titleThree: data ? data.titleThree : '',
-				titleThreeId: data ? data.titleThreeId : '',
-				buttonLableOne: data ? data.buttonLableOne : '',
-				buttonLableOneId: data ? data.buttonLableOneId : '',
-				buttonLableTwo: data ? data.buttonLableTwo : '',
-				buttonLableTwoId: data ? data.buttonLableTwoId : '',
-				buttonCustomClass: data ? data.buttonCustomClass : ''
-			}
-		});
-		dialogRef.afterClosed().subscribe((result) => {
-			this.onDialogCloseList.emit(result);
-		});
-	}
-	public openConfirmationDialogPlaceBid(data?) {
-		const dialogRef = this.dialogService.open(SukuConfirmationComponent, {
-			width: this.confirmationDialogWidth,
-			height: this.confirmationDialogHeight,
-			disableClose: this.confirmationDialogClose,
-			data: {
-				icon: data ? data.icon : '',
-				iconCustomClass: data ? data.iconCustomClass : '',
-				iconId: data ? data.iconId : '',
-				titleOne: data ? data.titleOne : '',
-				titleOneId: data ? data.titleOneId : '',
-				titleTwo: data ? data.titleTwo : '',
-				titleTwoId: data ? data.titleOneId : '',
-				titleThree: data ? data.titleThree : '',
-				titleThreeId: data ? data.titleThreeId : '',
-				buttonLableOne: data ? data.buttonLableOne : '',
-				buttonLableOneId: data ? data.buttonLableOneId : '',
-				buttonLableTwo: data ? data.buttonLableTwo : '',
-				buttonLableTwoId: data ? data.buttonLableTwoId : '',
-				buttonCustomClass: data ? data.buttonCustomClass : ''
-			}
-		});
-		dialogRef.afterClosed().subscribe((result) => {
-			this.onDialogClosePlaceBid.emit(result);
-		});
-	}
-	public openConfirmationDialogAcceptBid(data?) {
-		const dialogRef = this.dialogService.open(SukuConfirmationComponent, {
-			width: this.confirmationDialogWidth,
-			height: this.confirmationDialogHeight,
-			disableClose: this.confirmationDialogClose,
-			data: {
-				icon: data ? data.icon : '',
-				iconCustomClass: data ? data.iconCustomClass : '',
-				iconId: data ? data.iconId : '',
-				titleOne: data ? data.titleOne : '',
-				titleOneId: data ? data.titleOneId : '',
-				titleTwo: data ? data.titleTwo : '',
-				titleTwoId: data ? data.titleOneId : '',
-				titleThree: data ? data.titleThree : '',
-				titleThreeId: data ? data.titleThreeId : '',
-				buttonLableOne: data ? data.buttonLableOne : '',
-				buttonLableOneId: data ? data.buttonLableOneId : '',
-				buttonLableTwo: data ? data.buttonLableTwo : '',
-				buttonLableTwoId: data ? data.buttonLableTwoId : '',
-				buttonCustomClass: data ? data.buttonCustomClass : ''
-			}
-		});
-		dialogRef.afterClosed().subscribe((result) => {
-			this.onDialogCloseAcceptBid.emit(result);
-		});
-	}
-	public openConfirmationDialogSendPayment(data?) {
-		const dialogRef = this.dialogService.open(SukuConfirmationComponent, {
-			width: this.confirmationDialogWidth,
-			height: this.confirmationDialogHeight,
-			disableClose: this.confirmationDialogClose,
-			data: {
-				icon: data ? data.icon : '',
-				iconCustomClass: data ? data.iconCustomClass : '',
-				iconId: data ? data.iconId : '',
-				titleOne: data ? data.titleOne : '',
-				titleOneId: data ? data.titleOneId : '',
-				titleTwo: data ? data.titleTwo : '',
-				titleTwoId: data ? data.titleOneId : '',
-				titleThree: data ? data.titleThree : '',
-				titleThreeId: data ? data.titleThreeId : '',
-				buttonLableOne: data ? data.buttonLableOne : '',
-				buttonLableOneId: data ? data.buttonLableOneId : '',
-				buttonLableTwo: data ? data.buttonLableTwo : '',
-				buttonLableTwoId: data ? data.buttonLableTwoId : '',
-				buttonCustomClass: data ? data.buttonCustomClass : ''
-			}
-		});
-		dialogRef.afterClosed().subscribe((result) => {
-			this.onDialogCloseSendPayment.emit(result);
-		});
-	}
-	public openConfirmationDialogDeleteList(data?) {
-		const dialogRef = this.dialogService.open(SukuConfirmationComponent, {
-			width: this.confirmationDialogWidth,
-			height: this.confirmationDialogHeight,
-			disableClose: this.confirmationDialogClose,
-			data: {
-				icon: data ? data.icon : '',
-				iconCustomClass: data ? data.iconCustomClass : '',
-				iconId: data ? data.iconId : '',
-				titleOne: data ? data.titleOne : '',
-				titleOneId: data ? data.titleOneId : '',
-				titleTwo: data ? data.titleTwo : '',
-				titleTwoId: data ? data.titleOneId : '',
-				titleThree: data ? data.titleThree : '',
-				titleThreeId: data ? data.titleThreeId : '',
-				buttonLableOne: data ? data.buttonLableOne : '',
-				buttonLableOneId: data ? data.buttonLableOneId : '',
-				buttonLableTwo: data ? data.buttonLableTwo : '',
-				buttonLableTwoId: data ? data.buttonLableTwoId : '',
-				buttonCustomClass: data ? data.buttonCustomClass : ''
-			}
-		});
-		dialogRef.afterClosed().subscribe((result) => {
-			this.onDialogCloseDeleteList.emit(result);
-		});
-	}
-
-	public openConfirmationDialogCancelBid(data?) {
-		const dialogRef = this.dialogService.open(SukuConfirmationComponent, {
-			width: this.confirmationDialogWidth,
-			height: this.confirmationDialogHeight,
-			disableClose: this.confirmationDialogClose,
-			data: {
-				icon: data ? data.icon : '',
-				iconCustomClass: data ? data.iconCustomClass : '',
-				iconId: data ? data.iconId : '',
-				titleOne: data ? data.titleOne : '',
-				titleOneId: data ? data.titleOneId : '',
-				titleTwo: data ? data.titleTwo : '',
-				titleTwoId: data ? data.titleOneId : '',
-				titleThree: data ? data.titleThree : '',
-				titleThreeId: data ? data.titleThreeId : '',
-				buttonLableOne: data ? data.buttonLableOne : '',
-				buttonLableOneId: data ? data.buttonLableOneId : '',
-				buttonLableTwo: data ? data.buttonLableTwo : '',
-				buttonLableTwoId: data ? data.buttonLableTwoId : '',
-				buttonCustomClass: data ? data.buttonCustomClass : ''
-			}
-		});
-		dialogRef.afterClosed().subscribe((result) => {
-			this.onDialogCloseCancelBid.emit(result);
-		});
-	}
-
-	public openConfirmationDialogUpdateInterest(data?) {
-		const dialogRef = this.dialogService.open(SukuConfirmationComponent, {
-			width: this.confirmationDialogWidth,
-			height: this.confirmationDialogHeight,
-			disableClose: this.confirmationDialogClose,
-			data: {
-				icon: data ? data.icon : '',
-				iconCustomClass: data ? data.iconCustomClass : '',
-				iconId: data ? data.iconId : '',
-				titleOne: data ? data.titleOne : '',
-				titleOneId: data ? data.titleOneId : '',
-				titleTwo: data ? data.titleTwo : '',
-				titleTwoId: data ? data.titleOneId : '',
-				titleThree: data ? data.titleThree : '',
-				titleThreeId: data ? data.titleThreeId : '',
-				buttonLableOne: data ? data.buttonLableOne : '',
-				buttonLableOneId: data ? data.buttonLableOneId : '',
-				buttonLableTwo: data ? data.buttonLableTwo : '',
-				buttonLableTwoId: data ? data.buttonLableTwoId : '',
-				buttonCustomClass: data ? data.buttonCustomClass : ''
-			}
-		});
-		dialogRef.afterClosed().subscribe((result) => {
-			this.onDialogCloseUpdateInterest.emit(result);
+			this.onDialogClose.emit(result);
 		});
 	}
 }
