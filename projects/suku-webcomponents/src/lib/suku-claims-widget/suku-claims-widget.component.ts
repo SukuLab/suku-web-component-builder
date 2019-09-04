23d79a683ea950b4b4b91aee60807b701428087c
parashur <parashuram.b@comorin.co>
2019-09-04 20:05:15 +0530

    claims ui updates SukuLab/suku-program#949

---
+++ projects/suku-webcomponents/src/lib/suku-claims-widget/suku-claims-widget.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'suku-claims-widget',
+  templateUrl: './suku-claims-widget.component.html',
+  styleUrls: ['./suku-claims-widget.component.scss']
+})
+export class SukuClaimsWidgetComponent implements OnInit {
+  @Input() claims = [
+    {
+      name: 'Mas fresco',
+      verifiable: true
+    },
+    {
+      name: 'Mejor ternura',
+      verifiable: false
+    },
+    {
+      name: 'Mejor para medio ambiente y animal',
+      verifiable: false
+    }
+  ];
+  @Input() tooltipInfo = 'This claim has name an expired endorsement';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
