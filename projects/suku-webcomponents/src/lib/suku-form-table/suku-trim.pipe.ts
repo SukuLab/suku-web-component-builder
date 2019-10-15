d7344a3e67710d5927a834c4663a98851d038743
parashur <parashuram.b@comorin.co>
2019-10-15 15:18:06 +0530

    minor fix

---
+++ projects/suku-webcomponents/src/lib/suku-form-table/suku-trim.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'trim' })
+export class SukuTrimPipe implements PipeTransform {
+  transform(value: any) {
+    if (!value) {
+      return '';
+    }
+    console.log(value);
+    return value.replace(/\s/g, '');
+  }
+}
